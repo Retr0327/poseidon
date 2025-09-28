dd793b96190f6fdd42c6681b476a982ab98d1406
Retr0327 <retr0327.dev@gmail.com>
2025-09-28 13:48:00 +0800

    feat: implement upward directory search for project root

---
+++ internal/common/path/path.go
@@ -0,0 +1,20 @@
+package path
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func ProjectRootDir() string {
+	dir, _ := os.Getwd()
+	for {
+		if _, err := os.Stat(filepath.Join(dir, "go.mod")); err == nil {
+			return dir
+		}
+		parent := filepath.Dir(dir)
+		if parent == dir {
+			return ""
+		}
+		dir = parent
+	}
+}
